b41ac2dbb3d4e374768bf96f1538d0561c44114c
this.abhi98@gmail.com <luvkumar@luvs-MacBook-Air.local>
2025-08-27 20:08:04 +0530

    test_6_implemented and passed :to throws exception error whenone or more  negative number is passed

--- src/components/calculator-kata/CalculatorKata.js
+++ src/components/calculator-kata/CalculatorKata.js
@@ -19,6 +19,13 @@ function addString(numbersString) {
     .replace(new RegExp(delimiter, 'g'), ',')
     .split(',')
     .map(Number);
+
+    // Check for negative numbers and throw error if any
+    const negatives = splittedArray.filter(num => num < 0);
+    if(negatives.length > 0) {
+      throw new Error(`negatives not allowed: ${negatives.join(',')}`);
+    }
+
   var sum = splittedArray.reduce((acc, part) => acc + parseInt(part), 0);
   return sum
 }
