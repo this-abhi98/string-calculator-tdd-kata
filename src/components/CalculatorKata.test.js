eeea7aea2feba7be6ed9ce1baad2553816cf7ade
this.abhi98@gmail.com <luvkumar@luvs-MacBook-Air.local>
2025-08-28 01:34:02 +0530

    Final code refactoring

--- src/components/CalculatorKata.test.js
+++ src/components/CalculatorKata.test.js
@@ -1,28 +1,28 @@
-import { addString } from './calculator-kata/CalculatorKata';
+import { addString } from "./calculator-kata/CalculatorKata";
 
-describe('String Calculator kata', () => {
-    it('returns 0 for an empty string', () => {
-      expect(addString('')).toBe(0);
-    });
-    it('returns the number itself if only one number is provided', () => {
-      expect(addString('1')).toBe(1);
-    });
-    it('returns the sum of all the comma-separated numbers', () => {
-      expect(addString('1,2,3')).toBe(6);
-    });
-    it('returns the sum when newlines are used as delimiters as well', () => {
-      expect(addString('1,2,3\n5,4')).toBe(15);
-    });
-    it('should return sum on custom single-character delimiter using //[delimiter]\\n syntax', () => {
-      expect(addString('//;\n1;2')).toBe(3);
-    });
-    it('to throws exception error when a negative number is passed', () => {
-      expect(() => addString('1,-2')).toThrow('negatives not allowed: -2');
-    });
-    it('ignore numbers greater than 1000', () => {
-      expect(addString('2,1001')).toBe(2);
-    });
-    it('should supports custom delimiters of any length with //[***]\\n syntax', () => {
-    expect(addString('//[***]\n1***2***3')).toBe(6);
+describe("String Calculator kata", () => {
+  it("returns 0 for an empty string", () => {
+    expect(addString("")).toBe(0);
   });
-  })
+  it("returns the number itself if only one number is provided", () => {
+    expect(addString("1")).toBe(1);
+  });
+  it("returns the sum of all the comma-separated numbers", () => {
+    expect(addString("1,2,3")).toBe(6);
+  });
+  it("returns the sum when newlines are used as delimiters as well", () => {
+    expect(addString("1,2,3\n5,4")).toBe(15);
+  });
+  it("should return sum on custom single-character delimiter using //[delimiter]\\n syntax", () => {
+    expect(addString("//;\n1;2")).toBe(3);
+  });
+  it("to throws exception error when a negative number is passed", () => {
+    expect(() => addString("1,-2")).toThrow("negatives not allowed: -2");
+  });
+  it("ignore numbers greater than 1000", () => {
+    expect(addString("2,1001")).toBe(2);
+  });
+  it("should supports custom delimiters of any length with //[***]\\n syntax", () => {
+    expect(addString("//[***]\n1***2***3")).toBe(6);
+  });
+});
--- src/components/calculator-kata/CalculatorKata.js
+++ src/components/calculator-kata/CalculatorKata.js
@@ -1,6 +1,5 @@
-
 function addString(numbersString) {
-  if (numbersString === '') {
+  if (numbersString === "") {
     return 0;
   }
 
@@ -8,27 +7,27 @@ function addString(numbersString) {
   let delimiter = /,|\n/;
 
   // Check for custom delimiter
-  if (numbersString.startsWith('//')) {
-    const delimiterEndIndex = numbersString.indexOf('\n');
+  if (numbersString.startsWith("//")) {
+    const delimiterEndIndex = numbersString.indexOf("\n");
     delimiter = numbersString.substring(2, delimiterEndIndex);
     numbersString = numbersString.substring(delimiterEndIndex + 1);
   }
 
   // Replace custom delimiter inplace of comma and split the string
   const splittedArray = numbersString
-    .replace(new RegExp(delimiter, 'g'), ',')
-    .split(',')
+    .replace(new RegExp(delimiter, "g"), ",")
+    .split(",")
     .map(Number);
 
-    // Check for negative numbers and throw error if any
-    const negatives = splittedArray.filter(num => num < 0);
-    if(negatives.length > 0) {
-      throw new Error(`negatives not allowed: ${negatives.join(',')}`);
-    }
+  // Check for negative numbers and throw error if any
+  const negatives = splittedArray.filter((num) => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negatives not allowed: ${negatives.join(",")}`);
+  }
 
-    const filteredNumbers = splittedArray.filter((n) => n <= 1000);
+  // Ignore numbers greater than 1000
+  const filteredNumbers = splittedArray.filter((n) => n <= 1000);
 
-  var sum = filteredNumbers.reduce((acc, number) => acc + parseInt(number), 0);
-  return sum
+  return filteredNumbers.reduce((acc, number) => acc + parseInt(number), 0);
 }
-export { addString }
+export { addString };
