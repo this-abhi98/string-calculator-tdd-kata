9f01671bc6369e3851773b6adb381ec1e4c18cee
this.abhi98@gmail.com <luvkumar@luvs-MacBook-Air.local>
2025-08-27 19:17:58 +0530

    test_4_implemented and passed: returns the sum when newlines are used as delimiters as well

--- src/components/calculator-kata/CalculatorKata.js
+++ src/components/calculator-kata/CalculatorKata.js
@@ -3,7 +3,7 @@ function addString(numbersString) {
   if (numbersString === '') {
     return 0;
   }
-  const splittedArray = numbersString.split(',');
+  const splittedArray = numbersString.replace(/\n/g, ',').split(',');
   var sum = splittedArray.reduce((acc, part) => acc + parseInt(part), 0);
   return sum
 }
