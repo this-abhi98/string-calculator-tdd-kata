648498d81debf1df86398c45c8eb0b650d6cfd88
this.abhi98@gmail.com <luvkumar@luvs-MacBook-Air.local>
2025-08-27 18:39:31 +0530

    Test_2_Failed: returns the number itself if only one number is provided

--- src/components/CalculatorKata.test.js
+++ src/components/CalculatorKata.test.js
@@ -4,4 +4,7 @@ describe('String Calculator kata', () => {
     it('returns 0 for an empty string', () => {
       expect(addString('')).toBe(0);
     });
+    it('returns the number itself if only one number is provided', () => {
+      expect(addString('1')).toBe(1);
+    });
   })
